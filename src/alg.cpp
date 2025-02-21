6620abdc01dad9a2190976db6e470ce1ebda67b2
eliza3455 <catra7902@gmail.com>
2025-02-21 21:07:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,12 @@
 // Copyright 2021 NNTU-CS
 // Алгоритм Евклида
 
+
 int gcd(int a, int b) {
     while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
+      int temp = b;
+      b = a % b;
+      a = temp;
     }
-  return a; 
+    return a;
 }
-
